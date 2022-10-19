96a378030dd120a091888d82b60a0793dee4a4bb
thomas diaconu <thomas.diaconu@paprec.com>
2022-10-19 12:27:43 +0200

    add daily

--- top_k_freq_elements.py
+++ top_k_freq_elements.py
@@ -1,29 +1,25 @@
 from typing import List
+from heapq import *
 
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        h = []
+        heapify(h)
+        counter = {}
 
-def topKFrequent(nums: List[int], k: int) -> List[int]:
-    
-    n = len(nums)
-    counter, freq = {}, [[] for _ in range(n+1)]
-    
-    for val in nums:
-        counter[val] = counter.get(val, 0) + 1
-    
-    for elem in counter.keys():
-        freq[counter[elem]].append(elem)
-    
-    ans = []
-    
-    
-    for i in range(n, -1, -1):
-        if k == 0:
-            break
-        else:
-            if freq[i]:
-                for elem in freq[i]:
-                    ans.append(elem)
-                    k -= 1
-    return ans
+        for w in words:
+            counter[w] = counter.get(w, 0) + 1
+        
+        for key, val in counter.items():
+            heappush(h, (-val, key))
+        
+        res = []
+        while k > 0:
+            _, word = heappop(h)
+            res.append(word)
+            k -= 1
+        
+        return res
 
 '''
 T: O(n)
