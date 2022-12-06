4cb2cf4763d9707ac7a94cb3c714bb6c75422182
Thomas <thomas.diaconu@paprec.com>
2022-12-06 15:58:53 +0100

    add daily

--- odd_even_linked_list.py
+++ odd_even_linked_list.py
@@ -6,21 +6,22 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        if not head:
-            return head
+        if head:
+            odd, even, evenHead = head, head.next, head.next
+            while odd and even and even.next:
+                odd.next = even.next
+                even.next = even.next.next
+                odd = odd.next
+                even = even.next
+            odd.next = evenHead
         
-        odd, even, evenHead = head, head.next, head.next
-
-        while odd and even and even.next:
-            odd.next = even.next
-            even.next = even.next.next
-            odd = odd.next
-            even = even.next
-            
-        
-        odd.next = evenHead
         return head
+    
+'''
+T: O(n)
+S: O(1)
+'''
