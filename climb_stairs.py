07c52b835fdde7d04a86c4039907170c08f95b73
Thomas <thomas.diaconu@paprec.com>
2022-12-12 11:48:53 +0100

    add daily

--- climb_stairs.py
+++ climb_stairs.py
@@ -1,20 +1,16 @@
-def climbStairs(self, n: int) -> int:
-    
-    if n == 1:
-        return 1
-    
-    if n == 2:
-        return 2
-    
-    ways = [0] * (n)
-    
-    ways[0] = 1
-    ways[1] = 2
-    
-    for i in range(2,n):
-        ways[i] = ways[i-1] + ways[i-2]
-    
-    return ways[n-1]
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n <= 2:
+            return n
+
+        dp = [0] * n
+        dp[0], dp[1] = 1, 2
+
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[-1]
     
 
 '''
