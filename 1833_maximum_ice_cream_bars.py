611d831afe8087f24c61a613a7989fadba3b1d22
Thomas <58684405+thomandalorian@users.noreply.github.com>
2023-01-30 19:03:31 +0100

    Create 1833_maximum_ice_cream_bars.py

---
+++ 1833_maximum_ice_cream_bars.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        
+        costs.sort()
+        res, i = 0, 0
+
+        # Greedy approach
+        while i < len(costs) and coins > 0:
+            price = costs[i]
+            if price <= coins:
+                res += 1
+                i += 1
+                coins -= price
+            else:
+                break
+        return res
+        
+        '''
+        T: O(n)
+        S: O(1) --> no extra space
+        '''
