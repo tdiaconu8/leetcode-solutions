db94e7d25e587daac288ba09d41353c4daa1eea3
Thomas <58684405+thomandalorian@users.noreply.github.com>
2023-02-07 00:55:11 +0100

    Update 1470_shuffle_the_array.py

--- 1470_shuffle_the_array.py
+++ 1470_shuffle_the_array.py
@@ -1,6 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
 
+        # NEW ARRAY CREATION
         res = []
 
         for i in range(n):
@@ -9,7 +10,25 @@ class Solution:
         
         return res
 
+        # T: O(n)
+        # S: O(2n)
+
+        # NO EXTRA SPACE METHOD
+        # we store x, y values in the first half using bits calculation
         '''
-        T: O(n)
-        S: O(2n)
+        for i in range(n):
+            nums[i] = nums[i]<<10
+            nums[i] = nums[i] | nums[i+n]
+        
+        for i in range(n-1, -1, -1):
+            j = n-1-i
+            y = nums[i] & (2**10-1)
+            x = nums[i]>>10
+            nums[2*n-1-2*j] = y
+            nums[2*n-1-2*j-1] = x
+        
+        return nums
+
+        # T: O(2*n)
+        # S: O(1)
         '''
