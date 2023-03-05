e647ba19135aad0f0eb04e0ecb8eb2de8ca26b24
Thomas <58684405+thomandalorian@users.noreply.github.com>
2023-03-05 01:00:21 +0100

    Update 2444_subarray_with_fixed_bounds.py

--- 2444_subarray_with_fixed_bounds.py
+++ 2444_subarray_with_fixed_bounds.py
@@ -3,18 +3,18 @@ class Solution:
 
         n = len(nums)
         res = 0
-        l, prevMin, prevMax = -1, -1, -1
+        l, prevMin, prevMax = 0, -1, -1
 
         for i in range(n):
             num = nums[i]
             if not (minK <= num <= maxK):
-                l = i
+                l = i+1
             else:
                 if num == minK:
                     prevMin = i
                 if num == maxK:
                     prevMax = i
-                res += max(0, min(prevMin, prevMax)-l)
+                res += max(0, min(prevMin, prevMax)-l+1)
             
         return res
 
