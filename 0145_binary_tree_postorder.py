beb90a85e5f0f52f3f2077275e1f3c123713dc6c
Thomas <58684405+thomandalorian@users.noreply.github.com>
2023-03-16 17:59:26 +0100

    Create 0145_binary_tree_postorder.py

---
+++ 0145_binary_tree_postorder.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            dfs(node.right)
+            nonlocal res
+            res.append(node.val)
+        
+        dfs(root)
+        return res
+      
+      # T: O(n)
+      # S: O(n)
