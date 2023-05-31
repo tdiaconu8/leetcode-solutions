1e3a1a6844d015c65519daee1caf830c09b3b4d4
Thomas <58684405+thomandalorian@users.noreply.github.com>
2023-05-31 11:18:34 +0200

    Create 1603_design_parking_system.py

---
+++ 1603_design_parking_system.py
@@ -0,0 +1,21 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.places = {
+            1: big,
+            2: medium,
+            3: small
+        }
+        
+
+    def addCar(self, carType: int) -> bool:
+        if self.places[carType]:
+            self.places[carType] -= 1
+            return True
+        return False
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
