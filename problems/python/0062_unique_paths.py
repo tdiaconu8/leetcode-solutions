7e28fee49bdc1f24456727cc53c7474c60bc3f77
Thomas <thomas.diaconu94@gmail.com>
2023-09-03 19:36:58 +0200

    add daily

---
+++ problems/python/0062_unique_paths.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        # T: O(n*m)
+        # S: O(n*m)
+
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        for i in range(n):
+            dp[0][i] = 1
+        for j in range(m):
+            dp[j][0] = 1
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[-1][-1]
